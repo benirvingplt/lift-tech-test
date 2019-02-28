ae1fbd72b3eebd61656612df29f0ab7c355cd628
Rob Wilson <robwilson101@gmail.com>
2019-02-28 17:18:19 +0000

    Fix legend

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
         <Building />
         <div>
           <p>Call a lift by pressing the button on the desired floor.</p>
-          <p>0 lift not at floor</p>
-          <p>1 lift at floor but has no further destination</p>
-          <p>2 lift at floor and doors are opened</p>
+          <p>Red - lift not at floor</p>
+          <p>Amber -  lift at floor but has no further destination</p>
+          <p>Green - lift at floor and doors are opened</p>
         </div>
       </div>
     );
